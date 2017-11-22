3841c0fa7a8d7a7b4b4035ba0e7afd5f50ba7a2f
Douglas Martin <dkmartin@regenstrief.org>
2017-11-22 12:31:53 -0500

    Minor refactoring of theme support.

--- fujion-core/src/main/java/org/fujion/theme/ThemeResolvers.java
+++ fujion-core/src/main/java/org/fujion/theme/ThemeResolvers.java
@@ -20,11 +20,13 @@
  */
 package org.fujion.theme;
 
+import java.util.Iterator;
 import java.util.Set;
 import java.util.TreeSet;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
@@ -36,62 +38,101 @@ import org.springframework.web.servlet.ThemeResolver;
  * Delegates theme resolution to one or more registered theme resolvers.
  */
 public class ThemeResolvers implements BeanPostProcessor, ThemeResolver {
-
+    
     public static final String DEFAULT_THEME = "default";
+    
+    private static final String THEME_ATTR = ThemeResolvers.class.getName();
 
     private static final ThemeResolvers instance = new ThemeResolvers();
-    
+
     private final Set<ThemeResolver> themeResolvers = new TreeSet<>((tr1, tr2) -> {
         return getOrder(tr1) - getOrder(tr2);
     });
-    
+
     public static ThemeResolvers getInstance() {
         return instance;
     }
-    
+
     private ThemeResolvers() {
     }
-    
+
     private int getOrder(ThemeResolver themeResolver) {
         return themeResolver instanceof Ordered ? ((Ordered) themeResolver).getOrder() : Ordered.LOWEST_PRECEDENCE;
     }
-    
+
     @Override
     public String resolveThemeName(HttpServletRequest request) {
-        String themeName = null;
-        
-        for (ThemeResolver themeResolver : themeResolvers) {
-            themeName = themeResolver.resolveThemeName(request);
+        String themeName = getThemeName(request);
+        Iterator<ThemeResolver> iter = themeResolvers.iterator();
 
-            if (StringUtils.hasText(themeName)) {
-                break;
-            }
+        while (iter.hasNext() && !StringUtils.hasText(themeName)) {
+            themeName = iter.next().resolveThemeName(request);
+        }
+        
+        cacheThemeName(request, themeName);
+        return StringUtils.hasText(themeName) ? themeName : DEFAULT_THEME;
+    }
+    
+    /**
+     * Attempts to retrieve the name of the current theme. Tries the following, in order:
+     * <ol>
+     * <li>The request object</li>
+     * <li>A query parameter ("?theme=xxx")</li>
+     * <li>The session object</li>
+     * </ol>
+     *
+     * @param request The servlet request.
+     * @return The cached theme name, if any.
+     */
+    private String getThemeName(HttpServletRequest request) {
+        String themeName = (String) request.getAttribute(THEME_ATTR);
+        themeName = StringUtils.hasText(themeName) ? themeName : request.getParameter("theme");
+
+        if (!StringUtils.hasText(themeName)) {
+            HttpSession session = request.getSession(false);
+            themeName = session == null ? null : (String) session.getAttribute(THEME_ATTR);
         }
 
-        return StringUtils.hasText(themeName) ? themeName : DEFAULT_THEME;
+        return themeName;
+    }
+
+    /**
+     * Caches the theme name for efficiency.
+     *
+     * @param request The servlet request.
+     * @param themeName The theme name to cache.
+     */
+    private void cacheThemeName(HttpServletRequest request, String themeName) {
+        themeName = DEFAULT_THEME.equals(themeName) ? null : themeName;
+        request.setAttribute(THEME_ATTR, themeName);
+        HttpSession session = request.getSession(false);
+
+        if (session != null) {
+            session.setAttribute(THEME_ATTR, themeName);
+        }
     }
 
     @Override
     public void setThemeName(HttpServletRequest request, HttpServletResponse response, String themeName) {
         themeName = StringUtils.hasText(themeName) ? themeName : DEFAULT_THEME;
-        
+
         for (ThemeResolver themeResolver : themeResolvers) {
             themeResolver.setThemeName(request, response, themeName);
         }
     }
-
+    
     @Override
     public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
         return bean;
     }
-
+    
     @Override
     public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
         if (bean instanceof ThemeResolver) {
             themeResolvers.add((ThemeResolver) bean);
         }
-        
+
         return bean;
     }
-
+    
 }
