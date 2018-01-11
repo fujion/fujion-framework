3f4698fa22fa9d05f36b2d773d81bff1712f17d0
Douglas Martin <dkmartin@regenstrief.org>
2018-01-11 11:41:08 -0500

    Expose synchronized property on number box components.

--- fujion-core/src/main/java/org/fujion/component/BaseNumberboxComponent.java
+++ fujion-core/src/main/java/org/fujion/component/BaseNumberboxComponent.java
@@ -20,6 +20,8 @@
  */
 package org.fujion.component;
 
+import org.fujion.annotation.Component.PropertyGetter;
+import org.fujion.annotation.Component.PropertySetter;
 import org.springframework.util.NumberUtils;
 import org.springframework.util.StringUtils;
 
@@ -29,22 +31,38 @@ import org.springframework.util.StringUtils;
  * @param <T> The type of numeric value supported.
  */
 public abstract class BaseNumberboxComponent<T extends Number> extends BaseInputboxComponent<T> {
-
+    
     private final Class<T> clazz;
-
+    
     protected BaseNumberboxComponent(Class<T> clazz) {
         this.clazz = clazz;
     }
-
+    
+    @Override
+    @PropertyGetter(value = "synchronized", description = "A true value means that the client will notify the server "
+            + "as the value of the input box changes. A false value means that the client will notify "
+            + "server of the new value only when the input element loses focus.")
+    public boolean getSynchronized() {
+        return super.getSynchronized();
+    }
+    
+    @Override
+    @PropertySetter(value = "synchronized", defaultValue = "false", description = "A true value means that the client will notify the server "
+            + "as the value of the input box changes. A false value means that the client will notify "
+            + "server of the new value only when the input element loses focus.")
+    public void setSynchronized(boolean synchronize) {
+        super.setSynchronized(synchronize);
+    }
+    
     @Override
     protected String _toString(T value) {
         return value == null ? null : value.toString();
     }
-
+    
     @Override
     protected T _toValue(String value) {
         value = value == null ? "" : StringUtils.trimAllWhitespace(value);
         return value.isEmpty() ? null : NumberUtils.parseNumber(value, clazz);
     }
-
+    
 }
