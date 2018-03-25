941429d45817d1b754c58bb9ca69112ecd7d3273
Douglas Martin <dkmartin@regenstrief.org>
2018-03-24 23:39:32 -0400

    Add CKEditor component.

---
+++ fujion-ckeditor/src/main/java/org/fujion/ckeditor/CKEditor.java
@@ -0,0 +1,89 @@
+/*
+ * #%L
+ * fujion
+ * %%
+ * Copyright (C) 2008 - 2018 Regenstrief Institute, Inc.
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * #L%
+ */
+package org.fujion.ckeditor;
+
+import org.fujion.annotation.Component;
+import org.fujion.annotation.Component.PropertyGetter;
+import org.fujion.annotation.Component.PropertySetter;
+import org.fujion.component.BaseInputComponent;
+
+/**
+ * Fujion wrapper for CKEditor component.
+ */
+@Component(tag = "ckeditor", widgetModule = "fujion-ckeditor", widgetClass = "CKEditor", parentTag = "*", description = "CKEditor component.")
+public class CKEditor extends BaseInputComponent<String> {
+
+    private boolean readonly;
+    
+    private boolean sizable = true;
+    
+    /**
+     * Returns true if the input box is read-only.
+     *
+     * @return True if the input box is read-only.
+     */
+    @PropertyGetter(value = "readonly", description = "True if the input box is read-only.")
+    public boolean isReadonly() {
+        return readonly;
+    }
+
+    /**
+     * Sets the read-only state of the input box.
+     *
+     * @param readonly If true, the contents of the input box may not be changed by the user.
+     */
+    @PropertySetter(value = "readonly", defaultValue = "false", description = "True if the input box is read-only.")
+    public void setReadonly(boolean readonly) {
+        propertyChange("readonly", this.readonly, this.readonly = readonly, true);
+    }
+
+    /**
+     * Returns true if the editor may be resized manually.
+     *
+     * @return True if the editor may be resized manually.
+     */
+    @PropertyGetter(value = "sizable", description = "If true, a sizer grip allows manual resizing of the editor.  If false, the editor size matches the container size.")
+    public boolean isSizable() {
+        return sizable;
+    }
+
+    /**
+     * If set to true, the editor displays a sizing grip that can be used for manual resizing. If
+     * false, the editor matches the size of its container.
+     *
+     * @param sizable True if the editor may be resized manually.
+     */
+    @PropertySetter(value = "sizable", defaultValue = "true", description = "If true, a sizer grip allows manual resizing of the editor.  If false, the editor size matches the container size.")
+    public void setSizable(boolean sizable) {
+        propertyChange("sizable", this.sizable, this.sizable = sizable, true);
+    }
+    
+    @Override
+    protected String _toValue(String value) {
+        return value;
+    }
+
+    @Override
+    protected String _toString(String value) {
+        return value;
+    }
+
+}
---
+++ fujion-ckeditor/src/main/webjar/js/ckeditor.js
@@ -0,0 +1,133 @@
+'use strict';
+
+window.CKEDITOR_BASEPATH = fujion.baseurl + 'webjars/ckeditor/full/';
+
+define('fujion-ckeditor', [
+	'fujion-core', 
+	'fujion-widget',
+	'jshashes',
+	'ckeditor-full',
+	'fujion-ckeditor-css'], function(fujion, Widget, Hashes) { 
+	
+	/******************************************************************************************************************
+	 * A CKEditor widget
+	 ******************************************************************************************************************/ 
+	Widget.CKEditor = Widget.UIWidget.extend({
+	
+		MD5: new Hashes.MD5,
+		
+		/*------------------------------ Events ------------------------------*/
+		
+		blurHandler: function() {
+			if (this._changed && this.changed()) {
+				this._changed = false;
+				this.trigger('change', {value: this._editor.getData()});
+			}
+		},
+		
+		changeHandler: function() {
+			this._changed = true;
+			this.getState('synced') ? this.blurHandler() : null;
+		},
+		
+		instanceReadyHandler: function() {
+			this._ready = true;
+			this.syncState();
+			this.getState('sizable') ? null : this.resizeHandler();
+		},
+		
+		resizeHandler: function() {
+			this._ready ? this._editor.resize(this.widget$.width(), this.widget$.height()) : null;
+		},
+		
+		/*------------------------------ Lifecycle ------------------------------*/
+		
+		destroy: function() {
+			this.destroyEditor();
+			this._super();
+		},
+		
+		destroyEditor: function() {
+			if (this._editor) {
+				this._editor.destroy();
+				delete this._editor;
+			}
+		},
+		
+		init: function() {
+			this._super();
+			this.initState({sizable: true});
+			this.forwardToServer('change');
+		},
+		
+		/*------------------------------ Other ------------------------------*/
+		
+		changed: function() {
+			var hash = this.MD5.hex(this._editor.getData());
+			
+			if (!this._hash || this._hash !== hash) {
+				this._hash = hash;
+				this._changed = true;
+			} else {
+				this._changed = false;
+			}
+			
+			return this._changed;
+		},
+		
+		/*------------------------------ Rendering ------------------------------*/
+		
+		afterRender: function() {
+			this._super();
+			this._editor.once('instanceReady', this.instanceReadyHandler.bind(this));
+			this._editor.on('blur', this.blurHandler.bind(this));
+			this._editor.on('change', this.changeHandler.bind(this));
+			var iframe = this.widget$.find('iframe.fujion-resizer')[0];
+			
+			if (iframe) {
+				$(iframe.contentWindow).on('resize', this.resizeHandler.bind(this));
+			}
+		},
+		
+		beforeRender: function() {
+			this._super();
+			delete this._ready;
+			this.destroyEditor();
+			this._editor = CKEDITOR.replace(this.widget$.find('textarea')[0], {
+				baseHref: fujion.baseurl,
+				resize_enabled: this.getState('sizable')
+			});
+		},
+		
+		render$: function() {
+			var dom = this.getState('sizable') ? '<div><textarea/></div>' : '<div><textarea/><iframe class="fujion-resizer"/></div>';
+			return $(dom);
+		},
+		
+		/*------------------------------ State ------------------------------*/
+		
+		readonly: function(v) {
+			this._ready ? this._editor.setReadOnly(v) : null;
+		},
+		
+		sizable: function(v) {
+			this._syncing ? null : this.rerender();
+		},
+		
+		synced: function(v) {			
+		},
+		
+		value: function(v) {
+			if (this._ready) {
+				delete this._hash;
+				this._editor.setData(v);
+				this.changed();
+			}
+		}
+	});
+
+	
+	return fujion.widget;
+});
+
+
