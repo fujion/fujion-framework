77cd4e3337157abe78c2059a44135289876c7946
Douglas Martin <dkmartin@regenstrief.org>
2018-03-29 19:00:37 -0400

    Fix unit test.

--- fujion-icons-parent/fujion-icons-core/src/main/java/org/fujion/icon/IconLibraryBase.java
+++ fujion-icons-parent/fujion-icons-core/src/main/java/org/fujion/icon/IconLibraryBase.java
@@ -40,17 +40,17 @@ import org.springframework.util.Assert;
  * An abstract base implementation for webjar-packaged icon libraries.
  */
 public abstract class IconLibraryBase implements IIconLibrary, ApplicationContextAware {
-
+    
     private static final Log log = LogFactory.getLog(IconLibraryBase.class);
-
+    
     private final String defaultExtension;
-    
+
     private final WebJar webjar;
-
-    private final String[] dimensions;
-
-    private ResourcePatternResolver resolver;
     
+    private final String[] dimensions;
+    
+    private ResourcePatternResolver resolver;
+
     /**
      * Create icon library definition.
      *
@@ -64,29 +64,29 @@ public abstract class IconLibraryBase implements IIconLibrary, ApplicationContex
         this.dimensions = dimensions;
         this.defaultExtension = defaultExtension.startsWith(".") ? defaultExtension : "." + defaultExtension;
     }
-
+    
     @Override
     public String getId() {
         return webjar.getName();
     }
-
+    
     @Override
     public String getIconPath(String iconName, String dimensions) {
         return webjar.getRootPath() + webjar.getVersion() + "/" + formatPath(iconName, dimensions);
     }
-
+    
     @Override
     public List<String> getMatching(String iconName, String dims) {
         dims = resolveDims(dims);
         return dims == null ? Collections.emptyList() : getMatching(formatPath(iconName, dims));
     }
-    
+
     protected List<String> getMatching(String path) {
         List<String> matches = new ArrayList<>();
-
+        
         try {
-            path = webjar.getAbsolutePath() + path;
-
+            path = webjar.getAbsolutePath() + "**/" + path;
+            
             for (Resource resource : resolver.getResources(path)) {
                 if (resource.exists()) {
                     String p = resource.getURL().getPath();
@@ -97,30 +97,30 @@ public abstract class IconLibraryBase implements IIconLibrary, ApplicationContex
         } catch (IOException e) {
             log.error("Error enumerating icons.", e);
         }
-
+        
         return matches;
     }
-    
+
     @Override
     public String[] supportedDimensions() {
         return dimensions;
     }
-
+    
     protected String getDefaultExtension() {
         return defaultExtension;
     }
-
+    
     protected String resolveDims(String dims) {
         dims = StringUtils.isEmpty(dims) ? "*" : dims.toLowerCase();
-        
+
         if (ArrayUtils.contains(dimensions, dims)) {
             return dims;
         }
-        
+
         if (!dims.contains("x")) {
             dims += "x" + dims;
         }
-        
+
         if (!ArrayUtils.isEmpty(dimensions)) {
             for (String dim : dimensions) {
                 if (IconUtil.matcher.match(dims, dim)) {
@@ -128,24 +128,24 @@ public abstract class IconLibraryBase implements IIconLibrary, ApplicationContex
                 }
             }
         }
-        
+
         return null;
     }
-
+    
     protected String expandName(String name) {
         return name.contains(".") ? name : name + defaultExtension;
     }
-
+    
     private String formatPath(String name, String dims) {
         dims = resolveDims(dims);
         return dims == null ? null : doFormatPath(name, dims);
     }
-
+    
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) {
         resolver = applicationContext;
     }
-    
-    protected abstract String doFormatPath(String name, String dims);
 
+    protected abstract String doFormatPath(String name, String dims);
+    
 }
