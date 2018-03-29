c2f056117c2c3aa6e5454fd8631a705148a0be4d
Douglas Martin <dkmartin@regenstrief.org>
2018-03-29 16:40:05 -0400

    Add feather icon library.

---
+++ fujion-icons-parent/fujion-icons-feather/src/main/java/org/fujion/icon/feather/IconLibrary.java
@@ -0,0 +1,46 @@
+/* #%L
+ * fujion
+ * %%
+ * Copyright (C) 2008 - 2018 Regenstrief Institute, Inc.
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * #L%
+ */
+package org.fujion.icon.feather;
+
+import org.fujion.icon.IconLibraryBase;
+
+/**
+ * Icon library wrapper for Feather icon library.
+ */
+public class IconLibrary extends IconLibraryBase {
+    
+    /**
+     * Create icon library definition.
+     */
+    protected IconLibrary() {
+        super("feather-icons", "svg", "24x24");
+    }
+    
+    @Override
+    public String getId() {
+        return "feather";
+    }
+
+    @Override
+    protected String doFormatPath(String name, String dims) {
+        return "dist/icons/" + expandName(name);
+    }
+    
+}
