6aa8886166aceff786ba5fdaa7b196b73e0cd629
Douglas Martin <dkmartin@regenstrief.org>
2018-04-12 18:19:28 -0400

    Fix sporadic JRuby ArrayIndexOutOfBoundsException.
    Need to create scripting container with single thread scope.

--- fujion-script-parent/fujion-script-jruby/src/main/java/org/fujion/script/jruby/JRubyScript.java
+++ fujion-script-parent/fujion-script-jruby/src/main/java/org/fujion/script/jruby/JRubyScript.java
@@ -24,6 +24,7 @@ import java.util.Map;
 import java.util.Map.Entry;
 
 import org.fujion.script.IScriptLanguage;
+import org.jruby.embed.LocalContextScope;
 import org.jruby.embed.ScriptingContainer;
 import org.jruby.javasupport.JavaEmbedUtils.EvalUnit;
 
@@ -31,40 +32,40 @@ import org.jruby.javasupport.JavaEmbedUtils.EvalUnit;
  * Support for embedding Ruby scripts.
  */
 public class JRubyScript implements IScriptLanguage {
-    
+
     /**
      * Wrapper for a parsed Ruby script
      */
     public static class ParsedScript implements IParsedScript {
-        
+
         private final ScriptingContainer container;
-        
+
         private final EvalUnit unit;
-        
+
         public ParsedScript(String source) {
-            container = new ScriptingContainer();
+            container = new ScriptingContainer(LocalContextScope.SINGLETHREAD);
             unit = container.parse(source);
         }
-        
+
         @Override
         public Object run(Map<String, Object> variables) {
             container.clear();
-            
+
             if (variables != null) {
                 for (Entry<String, Object> entry : variables.entrySet()) {
                     container.put(entry.getKey(), entry.getValue());
                 }
             }
-            
+
             return unit.run();
         }
-        
+
         @Override
         public void destroy() {
             container.terminate();
         }
     }
-    
+
     /**
      * @see org.fujion.script.IScriptLanguage#getType()
      */
@@ -72,7 +73,7 @@ public class JRubyScript implements IScriptLanguage {
     public String getType() {
         return "jruby";
     }
-    
+
     /**
      * Global variables in JRuby must start with a '$' character.
      *
@@ -82,7 +83,7 @@ public class JRubyScript implements IScriptLanguage {
     public String getSelf() {
         return "$self";
     }
-    
+
     /**
      * @see org.fujion.script.IScriptLanguage#parse(java.lang.String)
      */
@@ -90,5 +91,5 @@ public class JRubyScript implements IScriptLanguage {
     public IParsedScript parse(String source) {
         return new ParsedScript(source);
     }
-    
+
 }
