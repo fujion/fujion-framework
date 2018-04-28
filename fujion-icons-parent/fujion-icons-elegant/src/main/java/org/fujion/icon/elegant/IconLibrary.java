646b58196e30eb4155e25291523c980614139765
Douglas Martin <dkmartin@regenstrief.org>
2018-04-28 13:17:19 -0400

    Add elegant and simple icon libraries.

---
+++ fujion-icons-parent/fujion-icons-elegant/src/main/java/org/fujion/icon/elegant/IconLibrary.java
@@ -0,0 +1,50 @@
+/* #%L
+ * fujion
+ * %%
+ * Copyright (C) 2008 - 2018 Regenstrief Institute, Inc.
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * #L%
+ */
+package org.fujion.icon.elegant;
+
+import org.apache.commons.io.FilenameUtils;
+import org.fujion.icon.IconLibraryBase;
+
+/**
+ * Icon library wrapper for Elegant icon collection.
+ */
+public class IconLibrary extends IconLibraryBase {
+    
+    /**
+     * Create icon library definition.
+     *
+     */
+    protected IconLibrary() {
+        super("github-com-paroos-elegant-icons", "png", "32x32");
+    }
+    
+    @Override
+    public String getId() {
+        return "elegant";
+    }
+
+    @Override
+    protected String doFormatPath(String name, String dims) {
+        name = expandName(name);
+        String ext = FilenameUtils.getExtension(name).toUpperCase();
+        return "images/" + ext + "/" + name;
+    }
+    
+}
---
+++ fujion-icons-parent/fujion-icons-elegant/src/test/java/org/fujion/icon/test/IconTests.java
@@ -0,0 +1,40 @@
+/* #%L
+ * fujion
+ * %%
+ * Copyright (C) 2008 - 2018 Regenstrief Institute, Inc.
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * #L%
+ */
+package org.fujion.icon.test;
+
+import org.junit.Test;
+
+public class IconTests extends IconTestBase {
+
+    @Test
+    public void test() {
+        setLibrary("elegant");
+        doTests();
+    }
+    
+    private void doTests() {
+        singleton("arrow_left", "32x32");
+        singleton("icon_star.png", "32");
+        singleton("icon_star.svg", "32x32");
+        multiple("arrow*.png", "*", 64);
+        multiple("arrow*.svg", "32x32", 64);
+    }
+    
+}
