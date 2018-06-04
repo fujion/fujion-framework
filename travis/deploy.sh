47d46b1aa441cd96a5900babfe697e36094c9768
Douglas Martin <dkmartin@regenstrief.org>
2018-06-04 12:24:19 -0400

    Minor change to deployment script.

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -3,4 +3,6 @@ if [[ "$TRAVIS_BRANCH" = 'master' ]] || [[ "$TRAVIS_BRANCH" =~ ^[0-9]+\.[0-9]+\.
   openssl aes-256-cbc -K $encrypted_7b99f14b867b_key -iv $encrypted_7b99f14b867b_iv -in travis/codesigning.asc.enc -out travis/codesigning.asc -d
   gpg --fast-import travis/codesigning.asc
   mvn -V -B -s travis/settings.xml clean deploy -P sign -DskipTests
+else
+  echo "Branch $TRAVIS_BRANCH not eligible for deployment."
 fi
