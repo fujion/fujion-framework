363293a9f4917d217e6f434deab04690aeacc97c
Douglas Martin <mdgeek1@gmail.com>
2018-08-08 09:11:50 -0400

    Add AbstractCache.clear method.

--- fujion-common/src/main/java/org/fujion/common/AbstractCache.java
+++ fujion-common/src/main/java/org/fujion/common/AbstractCache.java
@@ -30,109 +30,109 @@ import java.util.concurrent.locks.ReentrantLock;
 /**
  * Abstract class for managing globally cached data. Subclasses must implement the fetch logic for
  * retrieving the data object into the cache.
- * 
+ *
  * @param <KEY> The class of the indexing key.
  * @param <VALUE> The class of the cached item.
  */
 public abstract class AbstractCache<KEY, VALUE> implements Iterable<VALUE> {
-    
+
     private static class CachedObject<VALUE> {
-        
+
         private VALUE object;
-        
+
         private RuntimeException exception;
-        
+
         private ReentrantLock lock = new ReentrantLock();
-        
+
         private CachedObject() {
             lock.lock();
         }
-        
+
         void setObject(VALUE object) {
             this.object = object;
             removeLock();
         }
-        
+
         void setException(RuntimeException exception) {
             this.exception = exception;
             removeLock();
         }
-        
+
         VALUE getObject() {
             if (exception != null) {
                 throw exception;
             }
-            
+
             lock();
             unlock();
             return object;
         }
-        
+
         private void removeLock() {
             ReentrantLock lock = this.lock;
             this.lock = null;
             lock.unlock();
         }
-        
+
         private void lock() {
             if (lock != null) {
                 lock.lock();
             }
         }
-        
+
         private void unlock() {
             if (lock != null) {
                 lock.unlock();
             }
         }
     }
-    
+
     private final Map<KEY, CachedObject<VALUE>> map = new ConcurrentHashMap<>();
-    
+
     /**
      * Logic to retrieve the data item from its primary store based on the provided key. The
      * returned item will be stored in the cache for future retrieval. Note that it is acceptable to
      * return a null item from this call.
-     * 
+     *
      * @param key The key.
      * @return The fetched value.
      */
     protected abstract VALUE fetch(KEY key);
-    
+
     /**
      * Get value for specified key. If not in cache, will call subclass's fetch method and load into
      * cache.
-     * 
+     *
      * @param key The key.
      * @return The associated value.
      */
     public VALUE get(KEY key) {
         return isCached(key) ? map.get(key).getObject() : internalGet(key);
     }
-    
+
     /**
      * Returns true if the item associated with the specified key is in the cache.
-     * 
+     *
      * @param key The key.
      * @return True if associated item has been cached.
      */
     public boolean isCached(KEY key) {
         return map.containsKey(key);
     }
-    
+
     /**
      * Internal, thread-safe method for loading result into cache.
-     * 
+     *
      * @param key The key.
      * @return The associated value.
      */
     private VALUE internalGet(KEY key) {
         CachedObject<VALUE> cachedObject;
         boolean needsFetch;
-        
+
         synchronized (map) {
             needsFetch = !map.containsKey(key);
-            
+
             if (needsFetch) {
                 cachedObject = new CachedObject<VALUE>();
                 map.put(key, cachedObject);
@@ -140,7 +140,7 @@ public abstract class AbstractCache<KEY, VALUE> implements Iterable<VALUE> {
                 cachedObject = map.get(key);
             }
         }
-        
+
         if (needsFetch) {
             try {
                 cachedObject.setObject(fetch(key));
@@ -150,58 +150,65 @@ public abstract class AbstractCache<KEY, VALUE> implements Iterable<VALUE> {
                 throw e2;
             }
         }
-        
+
         return cachedObject.getObject();
     }
-    
+
     /**
      * Refresh the cache. Any existing entries in the cache will be re-fetched after it is cleared.
      */
     public void refresh() {
         synchronized (map) {
             Set<KEY> contents = new HashSet<>(map.keySet());
-            map.clear();
-            
+            clear();
+
             for (KEY key : contents) {
                 internalGet(key);
             }
         }
     }
-    
+
+    /**
+     * Clear the cache of all entries.
+     */
+    public void clear() {
+        map.clear();
+    }
+
     /**
      * Iterate over value set.
      */
     @Override
     public Iterator<VALUE> iterator() {
         return new Iterator<VALUE>() {
-            
+
             Iterator<CachedObject<VALUE>> iterator = map.values().iterator();
-            
+
             @Override
             public boolean hasNext() {
                 return iterator.hasNext();
             }
-            
+
             @Override
             public VALUE next() {
                 return iterator.next().getObject();
             }
-            
+
             @Override
             public void remove() {
                 throw new UnsupportedOperationException();
             }
-            
+
         };
     }
-    
+
     /**
      * Return number of entries.
-     * 
+     *
      * @return Number of entries.
      */
     public int size() {
         return map.size();
     }
-    
+
 }
