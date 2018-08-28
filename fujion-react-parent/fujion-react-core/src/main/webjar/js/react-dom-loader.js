1cc8768f795f2359788b4077d664a768bfca8c59
Douglas Martin <mdgeek1@gmail.com>
2018-08-28 08:01:49 -0400

    Move to Fujion-managed webjars.

--- fujion-react-parent/fujion-react-core/src/main/webjar/js/react-dom-loader.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-if (!fujion.debug) {
-  module.exports = require('webjars/react-dom/umd/react-dom.production.min.js');
-} else {
-  module.exports = require('webjars/react-dom/umd/react-dom.development.js');
-}
--- fujion-react-parent/fujion-react-core/src/main/webjar/js/react-loader.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-if (!fujion.debug) {
-  module.exports = require('webjars/react/umd/react.production.min.js');
-} else {
-  module.exports = require('webjars/react/umd/react.development.js');
-}
