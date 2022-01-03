1f620df878d74d1321e43fdbeb8e613a283ba499
Douglas Martin <mdgeek1@gmail.com>
2022-01-03 07:58:44 -0500

    Improve logic for finding bootstrap component from the module's decorators.

--- fujion-angular-parent/fujion-angular-core/src/main/angular/lib/src/bootstrap.ts
+++ fujion-angular-parent/fujion-angular-core/src/main/angular/lib/src/bootstrap.ts
@@ -1,12 +1,22 @@
 import {ApplicationRef, ComponentRef, DoBootstrap, NgModuleRef, NgZone} from '@angular/core';
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 
+/**
+ * Supports bootstrapping an Angular component.
+ *
+ * @param aModule The loaded module.
+ * @param selector The selector for the DOM element where the Angular component will be attached.
+ */
 export function AppContext(aModule: any, selector: string) {
 
     let zone: NgZone;
     let componentRef: ComponentRef<any>;
     let moduleRef: NgModuleRef<any>;
 
+    /**
+     * Angular module that will do the bootstrapping.  The decorators of the actual Angular module will be copied
+     * here to allow custom bootstrapping logic to be used.
+     */
     class AngularModule implements DoBootstrap {
         static decorators: any;
 
@@ -20,34 +30,60 @@ export function AppContext(aModule: any, selector: string) {
         throw new Error('No module named "AngularModule" was found');
     }
 
-    const decorators = aModule.AngularModule.decorators;
+    // Copy decorators from real Angular module to local module.
+    const decorators = aModule.AngularModule.decorators || [];
     AngularModule.decorators = decorators;
-    const ngModule = decorators?.[0]?.args?.[0];
+    let bootstrapComponent;
 
+    // Search for (and remove) any bootstrap component specified in a decorator.  This must be done or custom
+    // bootstrapping logic will not be executed.
+    for (const decorator of decorators) {
+        bootstrapComponent = decorator.args?.[0]?.bootstrap?.[0];
+
+        if (bootstrapComponent != null) {
+            delete decorator.args[0].bootstrap;
+            break;
+        }
+    }
+
+    // If an AngularComponent isn't explicitly specified, use the bootstrap component.
     if (aModule.AngularComponent == null) {
-        aModule.AngularComponent = ngModule?.bootstrap?.[0];
+        aModule.AngularComponent = bootstrapComponent;
 
         if (aModule.AngularComponent == null) {
             throw new Error('No Angular bootstrap component specified');
         }
     }
 
-    delete ngModule.bootstrap;
-
+    /**
+     * Returns true if the Angular module has been initialized (and, therefore, bootstrapping has occurred).
+     */
     AppContext.prototype.isLoaded = function (): boolean {
         return moduleRef != null;
     };
 
+    /**
+     * Bootstrap the Angular component.
+     */
     AppContext.prototype.bootstrap = function (): Promise<NgModuleRef<any>> {
         return platformBrowserDynamic().bootstrapModule(AngularModule).then(
             ref => moduleRef = ref);
     };
 
+    /**
+     * Destroy the module reference.
+     */
     AppContext.prototype.destroy = function (): void {
         moduleRef?.destroy();
         moduleRef = null;
     };
 
+    /**
+     * Invoke the named function on the bootstrapped component.  The invocation occurs within the component's zone.
+     *
+     * @param functionName The name of the function to invoke.
+     * @param args The function's arguments.
+     */
     AppContext.prototype.invoke = function (functionName: string, args: any[]): any {
         return zone.run(() => {
             let instance = componentRef.instance;
